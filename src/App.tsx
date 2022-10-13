248e061a0714f5a6854961cd8481283a6cd84774
Grazinascito <grazi.social84@gmail.com>
2022-10-12 21:35:41 -0300

    chore: tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -3,5 +3,5 @@ import "./style/global.css";
 export function App() {
   const [count, setCount] = useState(0);
 
-  return <h1>hello batata</h1>;
+  return <h1 className="text-2xl bg-cyan-500">hello batata</h1>;
 }
